1643e20b8de494a22946ef74f07156647d81b89d
HuguesSerrano <hugues.serrano@epitech.eu>
2023-10-04 16:42:20 +0200

    update

---
+++ ex_08/ex_08.php
@@ -0,0 +1,10 @@
+<?php
+
+function print_array(array $my_array) {
+    foreach ($my_array as $value) {
+        echo $value . "\n";
+    }
+}
+
+//$my_array = array("m16","m4","M249");
+//print_array($my_array);
