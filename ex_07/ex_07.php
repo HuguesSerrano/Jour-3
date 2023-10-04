997b7bfaf11dea3c1f80808c4b19e394ce15818b
HuguesSerrano <hugues.serrano@epitech.eu>
2023-10-04 15:57:46 +0200

    update

--- ex_07/ex_07.php
+++ ex_07/ex_07.php
@@ -5,7 +5,7 @@ function get_angry_dog($nbr) {
     for (;$nbr>0;$nbr=$nbr-1){
         $var = $var. "woof";
     };
-    return ($var);
+    return ($var); "\n";
 };
 
-echo get_angry_dog(5);
+//echo get_angry_dog(5);
