3d6373173243054d117b788f1df1743b2dede208
HuguesSerrano <hugues.serrano@epitech.eu>
2023-10-04 11:35:35 +0200

    update 1

---
+++ ex_01/ex_01.php
@@ -0,0 +1,16 @@
+<?php
+$integer = 42;
+$float =  42.42;
+$string = "forty-two" ;
+$bool = true;
+$nul = null ;
+
+/*
+echo($integer);
+echo($float);
+echo($string);
+echo($bool);
+echo($nul);
+*/
+
+?>
